f61c6984a129c72e4f08d8740f77e05f92a28891
SofiaS1604 <SofiaS1604@yandex.ru>
2020-08-05 22:40:15 +0300

    Add frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import RootPage from './view/App.vue';
+
+let VueTouch = require('vue-touch');
+
+Vue.use(VueTouch, {name: 'v-touch'});
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    // mode: "history",
+    routes: routes.routes
+});
+
+const App = new Vue({
+    el: '#app',
+    router,
+    name: 'App',
+    render: h => h(RootPage)
+});
---
+++ frontend/webpack.config.js
@@ -0,0 +1,75 @@
+const {VueLoaderPlugin} = require('vue-loader');
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'frontend/src', 'index.js'),
+    mode: "development",
+    output: {
+        publicPath: "/"
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: [
+                    {
+                        loader: "vue-loader",
+                        options: {
+                            transformAssetUrls: {
+                                source: './src/'
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['css-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [path.join(__dirname, 'src')]
+            },
+            {
+                test: /\.styl/,
+                use: [
+                    {
+                        loader: `style-loader`
+                    },
+                    {
+                        loader: `css-loader`
+                    },
+                    {
+                        loader: `stylus-loader`
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png|webp|gif|otf|ttf|woff|woff2|ani|eot|svg)$/,
+                loader: `url-loader`,
+                options: {
+                    name: `[name].[hash:20].[ext]`,
+                    limit: 10000
+                },
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new htmlWebpackPlugin({
+            filename: path.join(__dirname, `frontend/dist`, `index.html`),
+            template: path.join(__dirname, `frontend/public`, `index.html`),
+            inject: true,
+            minify: false
+        }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+    ]
+};
